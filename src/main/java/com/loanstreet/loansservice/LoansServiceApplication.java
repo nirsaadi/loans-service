451975f7c9aafddcffacc6ecb1eb1ca443ed8791
Nir Saadi <nirsaadi@gmail.com>
2021-04-04 22:38:21 -0400

    NS: added swagger

--- src/main/java/com/loanstreet/loansservice/LoansServiceApplication.java
+++ src/main/java/com/loanstreet/loansservice/LoansServiceApplication.java
@@ -3,8 +3,10 @@ package com.loanstreet.loansservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class LoansServiceApplication {
 
 	public static void main(String[] args) {
